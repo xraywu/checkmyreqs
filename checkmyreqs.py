9d33de69a01795da8888869987fa9992ba42d2fd
Xiaoli Wu <xraywu@Xiaolis-MacBook-Pro.local>
2017-02-05 16:15:04 +0800

    patch for https and added stop at error flag

--- checkmyreqs.py
+++ checkmyreqs.py
@@ -29,12 +29,12 @@ except ImportError:
 
 
 BASE_PATH = os.path.dirname(os.path.abspath(__file__))
-CLIENT = ServerProxy('http://pypi.python.org/pypi')
+CLIENT = ServerProxy('https://pypi.python.org/pypi')
 
 IGNORED_PREFIXES = ['#', 'git+', 'hg+', 'svn+', 'bzr+', '\n', '\r\n']
 
 
-def parse_requirements_file(req_file):
+def parse_requirements_file(req_file, stop_at_error):
     """
     Parse a requirements file, returning packages with versions in a dictionary
     :param req_file: requirements file to parse
@@ -56,11 +56,15 @@ def parse_requirements_file(req_file):
                 package_name, version = line.split('==')
                 packages[package_name] = version
             else:
-                print(TERMINAL.yellow('{} not pinned to a version, skipping'.format(line)))
+                if stop_at_error:
+                    sys.exit('{} does not have a valid version number'.format(line))
+                else:
+                    print(TERMINAL.yellow('{} does not have a valid version number'.format(line)))
 
     return packages
 
-def check_packages(packages, python_version):
+
+def check_packages(packages, python_version, stop_at_error):
     """
     Checks a list of packages for compatibility with the given Python version
     Prints warning line if the package is not supported for the given Python version
@@ -72,7 +76,7 @@ def check_packages(packages, python_version):
     """
 
     for package_name, package_version in packages.items():
-        print(TERMINAL.bold(package_name))
+        # print(TERMINAL.bold(package_name))
 
         package_info = CLIENT.release_data(package_name, package_version)
         package_releases = CLIENT.package_releases(package_name)
@@ -82,14 +86,16 @@ def check_packages(packages, python_version):
 
 
             # Some entries list support of Programming Language :: Python :: 3
-            # So we also want to check the major revision number of the version 
+            # So we also want to check the major revision number of the version
             # against the list of supported versions
             major_python_version = python_version.split('.')[0]
 
             if python_version in supported_pythons:
-                print(TERMINAL.green('compatible'))
+                pass
+                # print(TERMINAL.green('compatible'))
             elif major_python_version in supported_pythons:
-                print(TERMINAL.green('compatible'))
+                pass
+                # print(TERMINAL.green('compatible'))
             else:
                 latest_version = package_releases[0]
                 latest_package_info = CLIENT.release_data(package_name, latest_version)
@@ -102,21 +108,28 @@ def check_packages(packages, python_version):
 
                     if python_version in latest_supported_pythons:
                         upgrade_available = ' - update to v{} for support'.format(latest_version)
-
-                    print(TERMINAL.red('not compatible{}'.format(upgrade_available)))
+                    if stop_at_error:
+                        sys.exit('{}={} not compatible with Python {}{}'.format(package_name, package_version, python_version, upgrade_available))
+                    else:
+                        print(TERMINAL.red('{}={} not compatible with Python {}{}'.format(package_name, package_version, python_version, upgrade_available)))
                 else:
                     # We get here if there was not compatability information for
                     # the package version we requested
 
                     if python_version in latest_supported_pythons:
                         upgrade_available = ' - update to v{} for explicit support'.format(latest_version)
-
-                    print(TERMINAL.yellow('not specified{}').format(upgrade_available))
+                    if stop_at_error:
+                        sys.exit('{}={} version not specified{}'.format(package_name, package_version, upgrade_available))
+                    else:
+                        print(TERMINAL.yellow('{}={} version not specified{}'.format(package_name, package_version, upgrade_available)))
 
         else:
-            print(TERMINAL.red('not listed on pypi.python.org'))
+            if stop_at_error:
+                sys.exit('{}={} not available on pip'.format(package_name, package_version))
+            else:
+                print(TERMINAL.red('{}={} not available on pip'.format(package_name, package_version)))
 
-        print('-----')
+        # print('-----')
 
 
 def get_supported_pythons(package_info):
@@ -153,6 +166,11 @@ def main():
         help='Version of Python to check against. E.g. 2.5',
         default='.'.join(map(str, [sys.version_info.major, sys.version_info.minor]))
     )
+    parser.add_argument(
+        '-e', '--error', required=False,
+        help='Terminate the check and throw error msg when encounter warnings or errors',
+        action='store_true'
+    )
 
     args = parser.parse_args()
 
@@ -165,23 +183,23 @@ def main():
                 args_files = [open('requirements.txt')]
 
             except IOError:
-                print('Default file requirements.txt not found')
-                sys.exit(errno.ENOENT)
+                sys.exit('Default file requirements.txt not found')
     else:
         args_files = args.files
 
+    stop_at_error = args.error
+
     # Make sure Python version is in X.Y format
     if re.match('^[2-3].[0-9]$', args.python) is None:
-        print('Python argument invalid: Must be in X.Y format, where X is 2 or 3 and Y is 0-9')
-        sys.exit(errno.EINVAL)
+        sys.exit('Python argument invalid: Must be in X.Y format, where X is 2 or 3 and Y is 0-9')
 
-    print('Checking for compatibility with Python {}'.format(args.python))
+    print('Checking dependencies for compatibility with Python {}'.format(args.python))
 
     for filepath in args_files:
         print('{0}\r\n*****'.format(filepath.name))
 
-        packages = parse_requirements_file(filepath)
-        check_packages(packages, args.python)
+        packages = parse_requirements_file(filepath, stop_at_error)
+        check_packages(packages, args.python, stop_at_error)
         print('\n')
 
 
